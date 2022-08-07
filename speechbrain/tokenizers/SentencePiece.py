e12305daac604df822fbafc5051b2e254258c476
Hemanshu Bhargav <56455328+Hemanshu-Bhargav@users.noreply.github.com>
2022-08-07 12:38:21 -0400

    Revert vocab_size back to "vocab_size," rather than "vocab_size=500,"

--- speechbrain/tokenizers/SentencePiece.py
+++ speechbrain/tokenizers/SentencePiece.py
@@ -105,7 +105,7 @@ class SentencePiece:
     def __init__(
         self,
         model_dir,
-        vocab_size=500,
+        vocab_size,
         annotation_train=None,
         annotation_read=None,
         model_type="unigram",
